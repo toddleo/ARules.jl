7fa911c47b5f7ed22874e253c7d6af56436aa953
liulx <liulx@knownsec.com>
2015-01-05 16:58:13 +0800

    Packed ARules to module

---
+++ src/ARules.jl
@@ -0,0 +1,18 @@
+module ARules
+
+# export # common types and functions
+# FPTree,
+# FPEmptyNode,
+# FPTreeNode,
+# haschild,
+# growchild!,
+# climb_down!,
+# remove_node,
+# getnode
+
+include("common.jl")
+include("apriori.jl")
+include("fp_tree.jl")
+# include("fp_growth.jl")
+
+end # module end
